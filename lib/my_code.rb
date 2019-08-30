e0d0f1722fea01ede7dc9c0f10fa899f84ed572a
Austin Smith <trunk9slamchest@gmail.com>
2019-08-30 05:18:24 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,11 @@
 # Your Code Here
+
+def map(array)
+  new_arr = array.map { |e| yield(e) }
+  p new_arr
+end
+
+def reduce(array, start = 0)
+  array.all? { |e| e.is_a?(Integer) } ? val = array.reduce(start) { |e, n| yield(e, n) } : val = array.reduce() { |e, n| yield(e, n) }
+  p val
+end
