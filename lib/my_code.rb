8623d0966c172ccb7cdd503bb1be38e54930784d
Austin Smith <trunk9slamchest@gmail.com>
2019-09-01 01:55:42 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,6 +1,7 @@
 # Your Code Here
 
 def map(array)
+<<<<<<< HEAD
   new_arr = array.map { |e| yield(e) }
   p new_arr
 end
@@ -9,3 +10,15 @@ def reduce(array, start = 0)
   array.all? { |e| e.is_a?(Integer) } ? val = array.reduce(start) { |e, n| yield(e, n) } : val = array.reduce() { |e, n| yield(e, n) }
   p val
 end
+=======
+  new_arr = array.map { yield }
+  p new_arr
+  # yield
+  # new_arr
+end
+
+map([1, 2, 3, -9]){|n| n }
+# map(["paul", "gurney", "vladimir", "jessica", "chani"]){|n| n}
+# map([1, 2, 3, -9]){|n| n * 2}
+# map([1, 2, 3, -9]){|n| n * n}
+>>>>>>> 6d00d61fbb1f4452d3b3201331d0d2b3a35cfc79
